ecbde20184a24d0c24a6c78a9bf8ee0c103dceb8
남형석 <azerc@MacMini.local>
2022-04-12 19:35:23 +0900

    .. add inpose mode

--- client/src/components/GameApp.js
+++ client/src/components/GameApp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import Archer from "./models/Archer";
 import PlayGround from "./PlayGround";
 import styled from "styled-components";
 
@@ -15,6 +16,16 @@ function GameApp(props) {
     <>
       <CanvasContainer>
         <Canvas>
+          <ambientLight />
+          <directionalLight
+            position={[0, 20, 10]}
+            color="white"
+            intensity={0.5}
+          />
+          <pointLight position={[-1, 1, 3]} color="red" intensity={2} />
+          <pointLight position={[1, 1, 3]} color="blue" intensity={2} />
+          <pointLight position={[0, 3, -10]} color="white" intensity={2} />
+          <Archer />
           <PlayGround />
           <OrbitControls />
         </Canvas>
--- client/src/components/PlayGround.js
+++ client/src/components/PlayGround.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function PlayGround(props) {
   return (
-    <mesh>
+    <mesh rotation={[-Math.PI / 2, 0, 0]}>
       <planeBufferGeometry attach="geometry" args={[10, 10]} />
       <meshBasicMaterial attach="material" color="teal" />
       <axesHelper args={[10]} />
